9befe4a3ab3b6c56dc8812d4d29f1d141cec99c6
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2021-03-30 15:14:44 +0300

    context

--- src/views/Logout.js
+++ src/views/Logout.js
@@ -1,16 +1,21 @@
 /* eslint-disable no-unused-vars */
 import PropTypes from 'prop-types';
-import {useContext} from 'react';
+import {useContext, useEffect} from 'react';
 import {MediaContext} from '../contexts/MediaContext';
+import {Redirect} from 'react-router-dom';
 
 const Logout = ({history}) => {
   const [user, setUser] = useContext(MediaContext);
-  setUser(null);
-  localStorage.clear();
-  history.push('/');
+
+  useEffect(()=>{
+    setUser(null);
+    localStorage.clear();
+    // history.push('/');
+  }, []);
+
   return (
     <>
-      <h1>Logout</h1>
+      <Redirect to={'/'} />
     </>
   );
 };
